8c48e2a041fd4ed6149e74c3b8a6d4ac24205cc1
Peter Fraedrich <peter.fraedrich@coldblue-usa.com>
2015-08-05 12:51:23 -0400

    more goats

--- js_functions.js
+++ js_functions.js
@@ -6,22 +6,20 @@ var list_query = "https://www.googleapis.com/youtube/v3/search?part=snippet&mexr
 var vid_query = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id="
 var video_id = '' // set default video
 
-
-// this gets a list of videos from youtube
-function vid_list(length) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", list_query+length+apikey, false );
-	xmlHttp.send( null );
-	return xmlHttp.responseText;
-}
-
 // this gets details for a single video
 function vid_details(vid_id) {
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open( "GET", vid_query+vid_id+apikey, false );
 	xmlHttp.send( null );
 	return xmlHttp.responseText;
-}
+};
+
+function get_videos(length, page) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", list_query+length+page+'&type=video'+apikey, false );
+	xmlHttp.send( null );
+	return xmlHttp.responseText;
+};
 
 // this loads the video from youtube in an iframe
 function set_css(video_id) {
@@ -29,7 +27,7 @@ function set_css(video_id) {
 	var span = document.getElementById('video');
 	span.innerHTML = html;
 	document.getElementById('working').style.display = 'none';
-}
+};
 
 // goat css
 function goat() {
@@ -39,7 +37,16 @@ function goat() {
 	document.body.style.backgroundRepeat = 'repeat';
 	document.body.style.backgroundImage = 'url(' + gif + ')';
 	document.getElementById('wrapper').style.backgroundColor = 'rgba(255,255,255,0.0)';
-}
+};
+
+function taylor_swift() {
+	video_id = 'L26vEW8Xg2g';
+	set_css(video_id);
+	gif = 'http://media.giphy.com/media/Vw2uzhE0Bwfks/giphy.gif';
+	document.body.style.backgroundRepeat = 'repeat';
+	document.body.style.backgroundImage = 'url(' + gif + ')';
+	document.getElementById('wrapper').style.backgroundColor = 'rgba(255,255,255,0.0)';
+};
 
 function croc() {
 	video_id = 'q_qUiytLYRc';
@@ -51,88 +58,7 @@ function croc() {
 	document.getElementById('wrapper').style.backgroundImage = 'url(http://media.giphy.com/media/URZcG7uLd9h4s/giphy.gif)';
 	document.getElementById('wrapper').style.backgroundRepeat = 'repeat';
 	document.getElementById('wrapper').style.backgroundColor = 'rgba(255,255,255,0.0)';
-}
-/*
-// this is the main function 
-function vidsearch() {
-	// load input values from the html elements
-	var duration = document.getElementById('time_input').value;
-	// if goat, play goat video
-	if (duration == "goat" || duration == "goats") {
-		goat(); // trigger goat-themed CSS
-		video_id = "SjHUb7NSrNk";
-		set_css(video_id);
-	} else {
-	// do everything else
-		duration = duration.split(':'); // split form input into usable numbers
-		hour = parseInt(duration[0], 10); // get rid of leading zeroes
-		minute = parseInt(duration[1], 10); // ditto
-		second = parseInt(duration[2], 10); // ditto
-		if (hour == 0 || hour == 'NaN') {
-			// if no hour number, or hour = 0, then use just minutes and seconds
-			time = "PT" + minute + "M" + second + "S";
-		} else {
-			// otherwise use hours
-			time = "PT" + hour + "H" + minute + "M" + second + "S";
-		};
-		// get general duration for video (because the YT API is dumb)
-		if (hour < 1 && minute < 4) {
-			length = "&videoDuration=short";
-		} else if (hour < 1 && minute > 4 && minute < 20) {
-			length = "&videoDuration=medium";
-		} else if (hour < 1 && minute > 20 || hour > 0) {
-			length = "&videoDuration=long";
-		};
-		
-		// call vid_list(), get JSON response from YT and parse
-		vlist1 = JSON.parse(vid_list(length));
-		
-		// set up empty array for video id's
-		ids = [];
-		
-		// iterate through returned videos in JSON response
-		for (i in vlist1.items) {
-			// put the video id's into the array
-			ids.push(vlist1.items[i].id.videoId);
-		};
-		// get the details for each video from YT API
-		for (var i = 0; i < ids.length; i++) {
-			// vall vid_details(), parse as JSON
-			res = JSON.parse(vid_details(ids[i]));
-			// set var to the object we want in the JSON
-			duration = res.items[0].contentDetails.duration;
-			// check for matches
-			if (duration == time) {
-				video_id = res.items[0].id;
-				break; // if something matches, use that ID and exit the loop
-			} else {
-				// default to interior crocodile alligator video
-				video_id = 'q_qUiytLYRc';
-				// set wallpaper to stupid gif
-				gif = 'http://img.photobucket.com/albums/v717/Lauranoodle/AnimationAlligatorSkip_color.gif';
-				document.body.style.backgroundRepeat = 'repeat';
-				document.body.style.backgroundImage = 'url(' + gif + ')';
-				document.getElementById('wrapper').style.backgroundImage = 'url(http://media.giphy.com/media/URZcG7uLd9h4s/giphy.gif)';
-				document.getElementById('wrapper').style.backgroundRepeat = 'repeat';
-				document.getElementById('wrapper').style.backgroundColor = 'rgba(255,255,255,0.0)';
-				continue;
-			};
-		};
-	};
-	// update DOM with video iframe
-	set_css(video_id);	
-}; */
-
-function get_videos(length, page) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", list_query+length+page+'&type=video'+apikey, false );
-	xmlHttp.send( null );
-	return xmlHttp.responseText;
 };
-
-function show_working_bug() {
-	document.getElementById('working').style.display = 'block';
-}
 
 function vsearch2() {
 	document.getElementById('working').style.opacity = 1;
@@ -143,6 +69,8 @@ function vsearch2() {
 		goat();
 	} else if (duration == '') {
 		croc();
+	} else if (duration == 'taylor' || duration == 'swift' || duration == 'justin' || duration ==  'bieber' || duration == 'miley' || duration == 'cyrus') {
+		taylor_swift();
 	} else {
 		video_id = 'q_qUiytLYRc';
 		duration = duration.split(':'); // split form input into usable numbers
